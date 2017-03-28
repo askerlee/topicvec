10275573bf752e02e377b920f30b76229dae4076
U-DESKTOP-C72Q98D\Shaohua Li <shaohua@gmail.com>
2017-03-28 13:59:33 +0800

    v0.80

--- 20news.bat
+++ 20news.bat
@@ -1,4 +1,4 @@
 python topicExp.py -s 20news train
-python topicExp.py -i 20news-train-11314-sep281-em150-last.topic.vec 20news train,test
+python topicExp.py -i 20news-train-11314-sep281-em150-best.topic.vec 20news train,test
 python classEval.py 20news topicprop
 python classEval.py 20news topic-wvavg
--- reuters.bat
+++ reuters.bat
@@ -1,4 +1,4 @@
 python topicExp.py -s reuters train
-python topicExp.py -i reuters-train-5770-sep91-em150-last.topic.vec reuters train,test
+python topicExp.py -i reuters-train-5770-sep91-em150-best.topic.vec reuters train,test
 python classEval.py reuters topicprop
 python classEval.py reuters topic-wvavg
